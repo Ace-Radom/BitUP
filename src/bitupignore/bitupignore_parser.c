349f150044fc1f41b05c5784f1fc47ceb7a691a7
Ace_Radom <sichenradomlyu@gmail.com>
2023-02-28 08:37:07 +0100

    finish .bitupignore str readin-op.

--- src/bitupignore/bitupignore_parser.c
+++ src/bitupignore/bitupignore_parser.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<stdbool.h>
 
-#include"filesystem/dir.h"
+#include"filesystem/filesystem.h"
 
 #define __dirlist__ 1
 #define __bitignore__ 2
@@ -27,8 +28,6 @@ int main( int argc , char** argv ){
     strcpy( bitignore_dir , get_parent_dir( bitignore_path ) );
     // get .bitignore dir
 
-    printf( "%s\n%s\n%s\n" , dirlist_path , bitignore_path , bitignore_dir );
-
     FILE* dirlist = NULL;
     FILE* bitignore = NULL;
 
@@ -43,7 +42,60 @@ int main( int argc , char** argv ){
         return -1;
     } // open bitignore file
 
-    
+    char buf[1024];
+
+    while ( fgets( buf , sizeof( buf ) , bitignore ) != NULL )
+    {
+        int NoTabChars_BeginPos;
+        int NoTabChars_EndPos;
+        // this two int var contain the start and end pos of the str without tab chars
+
+        char* str_WithoutTabChars = ( char* ) malloc( strlen( buf ) );
+        // str without tab chars ('\t', '\0', '\n', ' ', etc.)
+
+        for ( NoTabChars_BeginPos = 0 ; NoTabChars_BeginPos < strlen( buf ) ; NoTabChars_BeginPos++ )
+        {
+            if ( buf[NoTabChars_BeginPos] == ' ' || buf[NoTabChars_BeginPos] == '\t' )
+            {
+                continue;
+            }
+            else
+            {
+                break;
+            }
+        } // get the begin pos of the str without tab chars
+
+        for ( NoTabChars_EndPos = strlen( buf ) ; NoTabChars_EndPos >= NoTabChars_BeginPos ; NoTabChars_EndPos-- )
+        {
+            if ( buf[NoTabChars_EndPos] == ' '  ||
+                 buf[NoTabChars_EndPos] == '\t' ||
+                 buf[NoTabChars_EndPos] == '\n' ||
+                 buf[NoTabChars_EndPos] == '\r' ||
+                 buf[NoTabChars_EndPos] == '\0'    )
+            {
+                continue;
+            }
+            else
+            {
+                break;
+            }
+        } // get the end pos of the str without tab chars
+
+        int str_WithoutTabChars_CopyPos;
+        int buf_CopyPos;
+        for ( buf_CopyPos = NoTabChars_BeginPos , str_WithoutTabChars_CopyPos = 0 ; buf_CopyPos <= NoTabChars_EndPos ; buf_CopyPos++ , str_WithoutTabChars_CopyPos++ )
+        {
+            str_WithoutTabChars[str_WithoutTabChars_CopyPos] = buf[buf_CopyPos];
+        }
+        str_WithoutTabChars[buf_CopyPos] = '\0';
+
+        printf( "%s\n" , str_WithoutTabChars );
+    }
+
+
+
+    fclose( dirlist );
+    fclose( bitignore );
 
     return 0;
 }
