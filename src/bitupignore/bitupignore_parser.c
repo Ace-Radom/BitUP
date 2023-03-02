d50a8c1af7ebade175dbfb17fb1bf43a422c5121
Ace_Radom <sichenradomlyu@gmail.com>
2023-03-02 10:35:27 +0100

    take the delete tab-chars program as a function 'clean_TabChars'

--- src/bitupignore/bitupignore_parser.c
+++ src/bitupignore/bitupignore_parser.c
@@ -4,9 +4,60 @@
 #include<stdbool.h>
 
 #include"filesystem/filesystem.h"
+#include"cvector/vector.h"
 
 #define __dirlist__ 1
 #define __bitignore__ 2
+
+char* clean_TabChars( char* __str ){
+    int NoTabChars_BeginPos;
+    int NoTabChars_EndPos;
+    // this two int var contain the start and end pos of the str without tab chars
+
+    char* str_WithoutTabChars = ( char* ) malloc( strlen( __str ) );
+    // str without tab chars ('\t', '\0', '\n', ' ', etc.)
+
+    for ( NoTabChars_BeginPos = 0 ; NoTabChars_BeginPos < strlen( __str ) ; NoTabChars_BeginPos++ )
+    {
+        if ( __str[NoTabChars_BeginPos] == ' ' || __str[NoTabChars_BeginPos] == '\t' )
+        {
+            continue;
+        }
+        else
+        {
+            break;
+        }
+    } // get the begin pos of the str without tab chars
+
+    for ( NoTabChars_EndPos = strlen( __str ) ; NoTabChars_EndPos >= NoTabChars_BeginPos ; NoTabChars_EndPos-- )
+    {
+        if ( __str[NoTabChars_EndPos] == ' '  ||
+             __str[NoTabChars_EndPos] == '\t' ||
+             __str[NoTabChars_EndPos] == '\n' ||
+             __str[NoTabChars_EndPos] == '\r' ||
+             __str[NoTabChars_EndPos] == '\0'    )
+        {
+            continue;
+        }
+        else
+        {
+            break;
+        }
+    } // get the end pos of the str without tab chars
+
+    unsigned short int str_WithoutTabChars_CopyPos;
+    unsigned short int __str_CopyPos;
+    for ( __str_CopyPos = NoTabChars_BeginPos , str_WithoutTabChars_CopyPos = 0 ; 
+          __str_CopyPos <= NoTabChars_EndPos ; 
+          __str_CopyPos++ , str_WithoutTabChars_CopyPos++ )
+    {
+        str_WithoutTabChars[str_WithoutTabChars_CopyPos] = __str[__str_CopyPos];
+    } // copy the str without tab chars from __str to new str var
+    str_WithoutTabChars[__str_CopyPos] = '\0';
+    // here: end the new str with '\0', otherwise there will be an unexpected error
+
+    return str_WithoutTabChars;
+} // function clean_TabChars
 
 int main( int argc , char** argv ){
 
@@ -42,68 +93,33 @@ int main( int argc , char** argv ){
         return -1;
     } // open bitignore file
 
-    char buf[1024];
+    c_vector dirlist_v;
+    c_vector bitignore_v;
+    cv_init( &dirlist_v , 10 );
+    cv_init( &bitignore_v , 10 );
+    // create and init vectors
 
-    while ( fgets( buf , sizeof( buf ) , bitignore ) != NULL )
+    char buf_dirlistReadIN[MAX_PATH_LENGTH_BY_BITUP];
+    char buf_bitignoreReadIN[1024];
+
+    while ( fgets( buf_dirlistReadIN , sizeof( buf_dirlistReadIN ) , dirlist ) != NULL )
     {
-
-        #pragma region delete_TabChars
-
-        int NoTabChars_BeginPos;
-        int NoTabChars_EndPos;
-        // this two int var contain the start and end pos of the str without tab chars
-
-        char* str_WithoutTabChars = ( char* ) malloc( strlen( buf ) );
-        // str without tab chars ('\t', '\0', '\n', ' ', etc.)
-
-        for ( NoTabChars_BeginPos = 0 ; NoTabChars_BeginPos < strlen( buf ) ; NoTabChars_BeginPos++ )
-        {
-            if ( buf[NoTabChars_BeginPos] == ' ' || buf[NoTabChars_BeginPos] == '\t' )
-            {
-                continue;
-            }
-            else
-            {
-                break;
-            }
-        } // get the begin pos of the str without tab chars
-
-        for ( NoTabChars_EndPos = strlen( buf ) ; NoTabChars_EndPos >= NoTabChars_BeginPos ; NoTabChars_EndPos-- )
-        {
-            if ( buf[NoTabChars_EndPos] == ' '  ||
-                 buf[NoTabChars_EndPos] == '\t' ||
-                 buf[NoTabChars_EndPos] == '\n' ||
-                 buf[NoTabChars_EndPos] == '\r' ||
-                 buf[NoTabChars_EndPos] == '\0'    )
-            {
-                continue;
-            }
-            else
-            {
-                break;
-            }
-        } // get the end pos of the str without tab chars
-
-        unsigned short int str_WithoutTabChars_CopyPos;
-        unsigned short int buf_CopyPos;
-        for ( buf_CopyPos = NoTabChars_BeginPos , str_WithoutTabChars_CopyPos = 0 ; 
-              buf_CopyPos <= NoTabChars_EndPos ; 
-              buf_CopyPos++ , str_WithoutTabChars_CopyPos++ )
-        {
-            str_WithoutTabChars[str_WithoutTabChars_CopyPos] = buf[buf_CopyPos];
-        } // copy the str without tab chars from buf to new str var
-        str_WithoutTabChars[buf_CopyPos] = '\0';
-        // here: end the new str with '\0', otherwise there will be an unexpected error
-
-        #pragma endregion delete_TabChars
-
-        printf( "%s\n" , str_WithoutTabChars );
+        cv_push_back( &dirlist_v , ( void* ) clean_TabChars( buf_dirlistReadIN ) );
     }
 
+    printf( "%d\n" , cv_len( &dirlist_v ) );
+    cv_print_str( &dirlist_v );
 
+    while ( fgets( buf_bitignoreReadIN , sizeof( buf_bitignoreReadIN ) , bitignore ) != NULL )
+    {
+        cv_push_back( &bitignore_v , ( void* ) clean_TabChars( buf_bitignoreReadIN ) );
+    }
+
+    printf( "%d\n" , cv_len( &bitignore_v ) );
+    cv_print_str( &bitignore_v );
 
     fclose( dirlist );
     fclose( bitignore );
-
+    
     return 0;
-}
+} // main
